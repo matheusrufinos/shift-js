e7dfe649dd10bc008dfb063241623340f790d0a1
Matheus Rufino <matheus.rufino@grupoboticario.com.br>
2022-08-26 21:19:08 -0300

    test: create shift method tests

--- challenge/matheusrufinos/__tests__/index.test.js
+++ challenge/matheusrufinos/__tests__/index.test.js
@@ -1,5 +1,16 @@
 const shift = require("../index");
 
 test("should get first array element", () => {
-  expect(shift(2)).toBe(2);
+  expect(shift(["avocado", "dog", "cat"])).toBe("avocado");
+});
+
+test("should decrease the array length", () => {
+  let array = ["soap", "beer", "barbecue"];
+  expect(array).toHaveLength(3);
+  shift(array);
+  expect(array).toHaveLength(2);
+});
+
+test("should receive undefined when array is empty", () => {
+  expect(shift([])).toBe(undefined);
 });
